3ead1cbfac0df80da7ba9b4e725f7e42d0a0ff14
MD. Shams Wadud <abbir.ku@gmail.com>
2019-09-08 07:35:29 +0600

    Add project files.

---
+++ AsyncAwaitExample/AsyncProcess.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExample
+{
+    public class AsyncProcess
+    {
+        public string GetHtml(string url)
+        {
+            var webClient = new WebClient();
+
+            return webClient.DownloadString(url);
+        }
+
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+
+        public void RunProcess(string url)
+        {
+            #region Total Watch
+            var watchTotal = System.Diagnostics.Stopwatch.StartNew();
+            #region Sync Process
+            var watchAP = System.Diagnostics.Stopwatch.StartNew();
+            var res = GetHtml(url);
+            Console.WriteLine(res.Substring(0, 100));
+            watchAP.Stop();
+            #endregion
+            #region Loop
+            var watchLoop = System.Diagnostics.Stopwatch.StartNew();
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i % 50 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            watchLoop.Stop();
+            #endregion
+            watchTotal.Stop();
+            #endregion
+            //Final Watch result
+            Console.WriteLine(string.Format("AP={0}, loop={1}, Total={2}",
+                watchAP.ElapsedMilliseconds,
+                watchLoop.ElapsedMilliseconds,
+                watchTotal.ElapsedMilliseconds));
+        }
+
+        public async Task RunProcessAsync(string url)
+        {
+            #region Total Watch
+            var watchTotal = System.Diagnostics.Stopwatch.StartNew();
+            #region Async Process
+            var watchAP = System.Diagnostics.Stopwatch.StartNew();
+            var htmlTask = GetHtmlAsync(url);
+            #region Loop
+            var watchLoop = System.Diagnostics.Stopwatch.StartNew();
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i%50==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            watchLoop.Stop();
+            #endregion
+            var res = await htmlTask;
+            Console.WriteLine(res.Substring(0, 100));
+            watchAP.Stop();
+            #endregion
+            watchTotal.Stop();
+            #endregion
+            //Final Watch result
+            Console.WriteLine(string.Format("AP={0}, loop={1}, Total={2}",
+                watchAP.ElapsedMilliseconds,
+                watchLoop.ElapsedMilliseconds,
+                watchTotal.ElapsedMilliseconds));
+        }
+    }
+}
---
+++ AsyncAwaitExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncProcess process = new AsyncProcess();
+            Task.Run(async () => { await process.RunProcessAsync("http://progressive.nikadevs.com/"); });
+            process.RunProcess("http://progressive.nikadevs.com/");
+            Console.ReadLine();
+        }
+    }
+}
